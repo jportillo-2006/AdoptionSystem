8243669e0671d86eac31fe334ab3fb74b6388589
Informatica <informatica@alumno.local>
2025-02-05 16:58:29 -0600

    Proyecto terminado

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -4,6 +4,8 @@ import { getUsers, getUserById, updateUser, deleteUser } from "./user.controller
 import { existeUsuarioById } from "../helpers/db-validator.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 import {uploadProfilePicture} from "../middlewares/multer-upload.js";
+import { tieneRole } from "../middlewares/validar-roles.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
@@ -33,6 +35,8 @@ router.put(
 router.delete(
     "/:id",
     [
+        validarJWT,
+        tieneRole("ADMIN_ROLE", "VENTAS_ROLE"),
         check("id", "No es un id valido").isMongoId(),
         check("id").custom(existeUsuarioById),
         validarCampos
